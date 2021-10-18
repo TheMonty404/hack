db8ef8e31d61d9fa54670bc3568a782b5f1232d9
Monty404 <82681184+TheMonty404@users.noreply.github.com>
2021-10-18 13:01:58 -0700

    Update rawen.js

--- rawen.js
+++ rawen.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = ".";
+const prefix = "monty";
 client.on("message", message => {
   if (message.content === ".") {
     if (message.channel.guild) {
